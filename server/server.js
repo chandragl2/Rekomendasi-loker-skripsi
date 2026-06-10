424ba5015fbe1bb09839e9464c2e29fe6ded9712
chandra <chandragulo77@gmail.com>
2026-06-10 11:38:10 +0700

    remove client static serving for railway

--- server/server.js
+++ server/server.js
@@ -66,14 +66,14 @@ const maybeStartScraperScheduler = () => {
   );
 };
 
-// Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+// // Serve static assets in production
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
-  });
-}
+//   app.get(/.*/, (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+//   });
+// }
 
 // Error Handler
 app.use(errorHandler);
